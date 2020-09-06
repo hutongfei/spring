d37bd9b24c1c2623fe7fefd1749f017aeb152966
hutongfei <13507469929@163.com>
2020-09-06 20:39:59 +0800

    Initial commit

---
+++ mytest/src/main/java/com/my/MainClass.java
@@ -0,0 +1,10 @@
+package com.my;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClass {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    }
+}
