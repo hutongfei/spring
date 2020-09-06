d0f1be03c7d22f2eddf06fad37f300663ab75e7f
hutongfei <13507469929@163.com>
2020-09-06 23:04:31 +0800

    。。。

--- mytest/src/main/java/com/my/MainClass.java
+++ mytest/src/main/java/com/my/MainClass.java
@@ -2,6 +2,10 @@ package com.my;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * 源码阅读指南
+ * https://zhuanlan.zhihu.com/p/81404427
+ */
 public class MainClass {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfiguration.class);
